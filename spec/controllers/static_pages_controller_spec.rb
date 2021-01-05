b61060487b4626e47f5426df9fdbf7efa29881d0
Naoki Ito <naoki.109.0525@gmail.com>
2021-01-05 01:09:27 +0000

    aboutテストを追記

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -25,4 +25,17 @@ it "returns a 200 response" do
     expect(response).to have_http_status "200"
   end
  end
-end
+ describe "about" do
+   #正常にレスポンスを返すこと
+  it "responds successfully" do
+    get :about
+    expect(response).to be_success
+  end
+  #200レスポンスを返すこと
+  it "returns a 200 response" do
+    get :about
+    expect(response).to have_http_status "200"
+  end
+ end
+ end
+
