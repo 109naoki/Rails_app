08c206519c899507679db6da4f4d41bbe256aab6
Naoki Ito <naoki.109.0525@gmail.com>
2021-01-08 06:57:51 +0000

    3章を終了しました

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
+  
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
   describe "#home" do
  # 正常にレスポンスを返すこと
  it "responds successfully" do
@@ -12,7 +15,13 @@ it "returns a 200 response" do
   get :home
   expect(response).to have_http_status "200"
   end
- end
+  # タイトルのテスト
+ it "has title 'Ruby on Rails Tutorial Sample App'" do
+   get :home
+      assert_select "title", "Home | #{base_title}"
+     end
+  end
+
  describe "help" do
    #正常にレスポンスを返すこと
    it "responds successfully" do
@@ -24,7 +33,13 @@ it "returns a 200 response" do
     get :help
     expect(response).to have_http_status "200"
   end
+   # タイトルのテスト
+ it "has title 'Ruby on Rails Tutorial Sample App'" do
+   get :help
+      assert_select "title", "Help | #{base_title}"
+     end
  end
+ 
  describe "about" do
    #正常にレスポンスを返すこと
   it "responds successfully" do
@@ -36,6 +51,11 @@ it "returns a 200 response" do
     get :about
     expect(response).to have_http_status "200"
   end
- end
+   # タイトルのテスト
+ it "has title 'Ruby on Rails Tutorial Sample App'" do
+   get :about
+      assert_select "title", "About | #{base_title}"
+     end
+  end
  end
 
