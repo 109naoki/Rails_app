8012c3694d54053e5bd290e5ed4c5cba912004d9
Naoki Ito <naoki.109.0525@gmail.com>
2021-01-13 02:17:36 +0000

    finish_form_with

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   context 'Get #new' do
     before {get signup_path}
     it 'responds successfully' do
