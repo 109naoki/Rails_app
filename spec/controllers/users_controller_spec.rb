0a35152615390f9a45ff88f233f954f774df9d79
Naoki Ito <naoki.109.0525@gmail.com>
2021-01-13 08:32:32 +0000

    finish-ch07

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,9 +4,24 @@ RSpec.describe UsersController, type: :request do
   let(:user) { FactoryBot.create(:user) }
 
   context 'Get #new' do
-    before {get signup_path}
-    it 'responds successfully' do
+    # 正常なレスポンスを返すこと
+    it "returns http success" do
+      get signup_path
+      expect(response).to be_success
       expect(response).to have_http_status 200
     end
+  end
+  
+  describe "Get #show" do
+    # ログイン済みのユーザとして
+    context "as an authenticated user" do
+      it "responds successfuly" do
+        sign_in_as user
+        get user_path(user)
+        expect(response).to be_success
+        expect(response).to have_http_status "200"
+      end
+    end
+    
   end
 end
