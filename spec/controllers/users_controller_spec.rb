e732ade68ad8da5049d50a3fa4aae9c23c573664
Naoki Ito <naoki.109.0525@gmail.com>
2021-01-17 06:59:15 +0000

    finish-ch08

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,27 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :request do
+RSpec.describe "User pages", type: :request do
   let(:user) { FactoryBot.create(:user) }
 
-  context 'Get #new' do
-    # 正常なレスポンスを返すこと
-    it "returns http success" do
-      get signup_path
-      expect(response).to be_success
-      expect(response).to have_http_status 200
-    end
-  end
-  
-  describe "Get #show" do
-    # ログイン済みのユーザとして
+  # newアクションのテストは省略
+
+  describe "GET #show" do
+
+    # ログイン済みのユーザーとして
     context "as an authenticated user" do
-      it "responds successfuly" do
+      # 正常なレスポンスを返すこと
+      it "responds successfully" do
         sign_in_as user
         get user_path(user)
         expect(response).to be_success
         expect(response).to have_http_status "200"
       end
     end
-    
+
+    # ログインしていないユーザーの場合
+    context "as a guest" do 
+      # ログイン画面にリダイレクトすること
+      it "redirects to the login page" do
+        get user_path(user)
+        expect(response).to redirect_to login_path
+      end
+    end
+
+    # アカウントが違うユーザーの場合
+    context "as other user" do 
+      before do
+        @other_user = FactoryBot.create(:user)
+      end
+
+      # ホーム画面にリダイレクトすること
+      it "redirects to the login page" do
+        sign_in_as @other_user
+        get user_path(user)
+        expect(response).to redirect_to root_path
+      end
+    end
   end
 end
